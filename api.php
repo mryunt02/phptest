e72fab2a015ae50399505de04505ab544485ee0a
mryunt02 <mrthresh58@gmail.com>
2024-06-16 16:28:12 +0300

    Access Control

--- api.php
+++ api.php
@@ -2,6 +2,7 @@
 include_once "classes/converter.php";
 
 header("Content-Type: application/json"); // set the header to json
+header("Access-Control-Allow-Origin: *"); // allow all origins
 $code = $_GET["code"] ?? "BTC"; // get the value of code from the query string
 $converter = new CryptoConverter($code);
 $rateInUsd = $converter->convert(1);
